1002f12fb26a00c6c80c7944016b11bbb604edc3
Emmapalmkvist <palmkvistemma@hotmail.com>
2018-05-09 14:20:55 +0200

    test på administrator overview

--- app/src/main/java/com/example/thedrinkbook/Notifications.java
+++ app/src/main/java/com/example/thedrinkbook/Notifications.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 /**
@@ -21,7 +22,18 @@ public class Notifications {
             String description = "Notifikation fra DrinkBook";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-
+            channel.setDescription(description);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+       //     NotificationManager.createNotificationChannel(channel);
         }
+
+        createIntent(context);
+    }
+
+    private void createIntent(Context context) {
+        Intent notificationIntent = new Intent(context, AdminstratorOverViewActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
     }
 }
